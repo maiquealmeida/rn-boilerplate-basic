a2d5231252cc270391e1ef787d4a9fd9e27929a4
Maique Almeida <maique.malmeida@gmail.com>
2019-03-14 08:41:27 -0400

    Correção de erros e melhorias nos arquivos de fonte do Android

--- src/screens/Detail/index.js
+++ src/screens/Detail/index.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import styles from './styles';
 
 export default class Detail extends Component {
-  handlePress = () => this.props.navigation.navigate('Detail');
+  handlePress = () => this.props.navigation.navigate('Main');
 
-  Render() {
+  render() {
     return (
       <View style={styles.container}>
         <Icon name="thumbs-o-up" size={30} color="#005500" />
         <Text>Welcome to Detail.</Text>
-        <TouchableOpacity onPress={() => this.handlePress}>
-          <Text>View Detail</Text>
+        <TouchableOpacity onPress={this.handlePress}>
+          <Text>Back to Main</Text>
         </TouchableOpacity>
       </View>
     );
--- src/screens/Main/index.js
+++ src/screens/Main/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import styles from "./styles";
 
